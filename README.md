135956ce78db47ef8d6dacbb7cb3a584a773d65e
MWehrstedt <marcel.wehrstedt@outlook.com>
2024-09-08 15:50:35 +0200

    Initial Setup

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # ProjectOwls
+Think Mega Man bosses, but with a boxer. Made for DMG. It aims to run on original hardware and uses MBC5.
+
+# Compile
+Adjust paths to GDBK and your favourite emulator if necessary and run the make file or `compile.bat`. You can use `compile.bat -nostart` To just compile the game.
+
+Or just use the *makefile*.
+
+## Frameworks
+Made with [GBDK 2020 version 4.3.0](https://github.com/gbdk-2020/gbdk-2020)
---
+++ compile.bat
@@ -0,0 +1,49 @@
+@echo off
+
+REM Compile Script. Basically does, what the Makefile does, just manually.
+
+mkdir -p obj
+
+SET COMP_STRING=
+SET GAME_TITLE="OWLS "
+SET GAME_RESULT=powl.gb
+
+SETLOCAL EnableDelayedExpansion
+
+ECHO === Start Compile %GAME_RESULT% ===
+
+REM resource files
+ECHO -- Compiling resource files --
+for /f %%f in ('dir res\SFX\*.c /b') do (
+    bin\lcc -c -o obj\%%~nf.o res\SFX\%%~nf.c -debug
+    ECHO res\SFX\%%f
+    SET COMP_STRING=!COMP_STRING! obj\%%~nf.o
+)
+
+for /f %%f in ('dir res\*.c /b') do (
+    bin\lcc -c -o obj\%%~nf.o res\%%~nf.c -debug
+    ECHO res\%%f
+    SET COMP_STRING=!COMP_STRING! obj\%%~nf.o
+)
+
+REM source files
+ECHO -- Compiling source files --
+for /f %%f in ('dir src\*.c /b') do (
+    bin\lcc -c -o obj\%%~nf.o src\%%~nf.c -debug
+    ECHO res\%%f
+    SET COMP_STRING=!COMP_STRING! obj\%%~nf.o
+)
+
+ECHO obj\%GAME_RESULT%
+bin\lcc -o obj\%GAME_RESULT% %COMP_STRING% -Wm-yn%GAME_TITLE% -Wl-yt0x19 -Wl-j -Wm-yoA -autobank -Wb-ext=.rel -Wb-v -debug
+rem bin\lcc -o obj\%GAME_RESULT% %COMP_STRING% res/hUGEDriver.lib -Wm-yn%GAME_TITLE% -debug
+
+ECHO %COMP_STRING%
+ECHO === End Compile %GAME_RESULT% ===
+
+IF "%~1"=="-nostart" GOTO SCRIPTEND
+
+ECHO -- Start ROM in BGB --
+..\..\Tools\bgb\bgb.exe obj\%GAME_RESULT%
+
+:SCRIPTEND
